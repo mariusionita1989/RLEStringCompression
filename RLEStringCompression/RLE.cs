867556916b61dca6c635cca3f23ddaf5eda37875
Marius <marius.ionita.portfolio@outlook.com>
2023-12-22 01:07:53 +0200

    Add project files.

---
+++ RLEStringCompression/RLE.cs
@@ -0,0 +1,68 @@
+﻿using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace RLEStringCompression
+{
+    public static class RLE
+    {
+        // the fastest
+        #region V1
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static string CompressV1(string input)
+        {
+            if (string.IsNullOrEmpty(input))
+                return string.Empty;
+
+            int length = input.Length;
+            var compressed = new StringBuilder(length * 2);
+
+            int i = 0;
+            while (i < length)
+            {
+                char currentChar = input[i];
+                int count = 1;
+
+                // Incrementing 'i' separately for efficiency
+                int nextIndex = i + 1;
+                while (nextIndex < length && input[nextIndex] == currentChar)
+                {
+                    count++;
+                    nextIndex++;
+                }
+
+                compressed.Append(count).Append(currentChar);
+                i = nextIndex;
+            }
+
+            return compressed.ToString();
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static string DecompressV1(string input)
+        {
+            if (string.IsNullOrEmpty(input))
+                return string.Empty;
+
+            int length = input.Length;
+            var decompressed = new StringBuilder(length);
+
+            int i = 0;
+            while (i < length)
+            {
+                int count = input[i] - '0';
+                char character = input[i + 1];
+
+                // Ensure the capacity in chunks to minimize reallocations
+                decompressed.EnsureCapacity(decompressed.Length + (count << 2));
+
+                for (int j = 0; j < count; j++)
+                    decompressed.Append(character);
+
+                i += 2;
+            }
+
+            return decompressed.ToString();
+        }
+        #endregion
+    }
+}
---
+++ RLEStringCompression/RLEStringCompressionDemo.cs
@@ -0,0 +1,32 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Order;
+using System.Text;
+
+namespace RLEStringCompression
+{
+    [MemoryDiagnoser]
+    [Orderer(SummaryOrderPolicy.FastestToSlowest)]
+    [RankColumn]
+    public class RLEStringCompressionDemo
+    {
+        private const int length = 1024 * 1024 * 32;
+        private StringBuilder? sb;
+        private string output = string.Empty;
+
+        [GlobalSetup]
+        public void GlobalSetup()
+        {
+            sb = new StringBuilder(length);
+        }
+
+        [Benchmark(Baseline = true)]
+        public void RLE_V1()
+        {
+            if (sb != null) 
+            {
+                output = RLE.CompressV1(sb.ToString());
+                RLE.DecompressV1(output);
+            } 
+        }
+    }
+}
